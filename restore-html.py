0545ebe519946978bcfedcc6d1c4a712ccab7f3f
le <dropmemail@inbox.ru>
2023-06-28 14:42:33 +0300

    add beatufull soup parsing title

--- restore-html.py
+++ restore-html.py
@@ -4,6 +4,7 @@ from scapy.all import rdpcap
 import gzip
 import zlib
 import brotli
+from bs4 import BeautifulSoup
 
 import argparse
 import re
@@ -14,28 +15,30 @@ def get_tcp_session(packets):
     for packet in packets:
         if TCP in packet and packet[TCP].payload:
             session = (packet[TCP].sport, packet[TCP].dport)
-            sessions[session] = (sessions.get(session, b'') + b'\r\n\r\n' + packet[TCP].payload.load
-                                ).replace(b'\r\n\r\n\r\n\r\n',b'\r\n\r\n')
-            
+            sessions[session] = re.sub(
+                b'(\r\n\r\n)+', b'\r\n\r\n',
+                sessions.get(session, b'') + b'\r\n\r\n' + packet[TCP].payload.load)            
 
     return sessions
-
 
 def get_http_content(sessions):
     contents = {}
 
     for session, payload in sessions.items():
         while b'\r\n\r\n' in payload:
+            
             header, payload = payload.split(b'\r\n\r\n', 1)
-            # print(f"header: {header[:100]}\npayload: {payload[:100]}\nsession: {session}")
-            # input()
-            if len(payload) and b'HTTP' in payload[:100]:
-                continue
+            
+            # if b'HTTP' not in header: # and len(payload) and b'HTTP' in payload[:50]:
+            #     continue
 
-            if b'\r\n\r\n' in payload:
-                body, payload = payload.split(b'\r\n\r\n', 1)
-            else:
-                body = payload
+            # print(f"header: {header}\npayload: {payload[:100]}\nsession: {session}")
+            # input()
+            
+            # if b'\r\n\r\n' in payload:
+            #     body, payload = payload.split(b'\r\n\r\n', 1)
+            # else:
+            #     body = payload
             
             enc_match = re.search(b'Content-Encoding: ([^\r\n]*)', header)
 
@@ -47,30 +50,29 @@ def get_http_content(sessions):
                 # input()
                 try:
                     if encoding == 'gzip':
-                        contents[session] = contents.get(session, '') + gzip.decompress(body).decode('utf-8')
+                        contents[session] = contents.get(session, '') + gzip.decompress(payload).decode('utf-8')
                     elif encoding == 'deflate':
-                        contents[session] = contents.get(session, '') + zlib.decompress(body).decode('utf-8')
+                        contents[session] = contents.get(session, '') + zlib.decompress(payload).decode('utf-8')
                     elif encoding == 'br':
-                        contents[session] = contents.get(session, '') + brotli.decompress(body).decode('utf-8')
+                        contents[session] = contents.get(session, '') + brotli.decompress(payload).decode('utf-8')
                     else:
-                        contents[session] = contents.get(session, '') + body.decode('utf-8')
+                        contents[session] = contents.get(session, '') + payload.decode('utf-8')
                 except Exception as e:
                     continue
             else:
                 try:
-                    contents[session] = contents.get(session, '') + body.decode('utf-8')
+                    contents[session] = contents.get(session, '') + payload.decode('utf-8')
                 except Exception as e:
                     continue
 
     return contents
 
-def get_html_content(contents):
-    html_contents = {}
+def save_content(contents):
     for session, content in contents.items():
-        if "<!doctype html>" in content.lower():
-            html_contents[session] = content
-    return html_contents
-
+        soup = BeautifulSoup(content)
+        filename = f"{soup.title.string}.html" if soup.title else f"session_{session[0]}-{session[1]}.out"
+        with open(f"{filename}", "w") as f:
+            f.write(content)
 
 parser = argparse.ArgumentParser(
     description='Restore HTML payload from TCP packets'
@@ -90,7 +92,9 @@ packets = rdpcap(args.input_file)[TCP]
 
 contents = get_html_content(get_http_content(get_tcp_session(packets)))
 
-for session in contents:
-    with open(f"{args.output_prefix}{session[0]}-{session[1]}.html", "w", encoding="utf-8") as f:
-        f.write(contents[session])
+for session, content in contents.items():
+    soup = BeautifulSoup(content)
+    filename = f"{soup.title.string}.html" if soup.title else f"session_{session[0]}-{session[1]}.out"
+    with open(f"{args.output_prefix}{filename}", "w", encoding="utf-8") as f:
+        f.write(content)
 
